1c57eb026d1d0efbe103edc940d00de23a298dcf
HaRi-Haran-D <hvj58128@gmail.com>
2025-12-30 23:12:17 +0530

    customer app modified

--- proinvent/customers/views.py
+++ proinvent/customers/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'customers/base.html')
+
+def cart(request):
+    pass
+    #return render(request, 'customers/cart.html')
